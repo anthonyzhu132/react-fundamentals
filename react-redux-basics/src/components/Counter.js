0ef662bdad0243dcfb28d164788a514ff76fd7db
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-03-13 22:12:00 -0700

    added toggle counting dispatch

--- react-redux-basics/src/components/Counter.js
+++ react-redux-basics/src/components/Counter.js
@@ -8,6 +8,8 @@ const Counter = () => {
   //automatically subscribes and will reload if the state changes
   const counter = useSelector(state => state.counter);
 
+  const toggle = useSelector(state => state.showCounter);
+
   //Will dispatch an action to the store that we are subscribed to
   const dispatch = useDispatch();
 
@@ -19,14 +21,21 @@ const Counter = () => {
     dispatch({ type: 'decrement' })
   }
 
-  const toggleCounterHandler = () => {};
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', payload: 5 })
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' })
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {toggle && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>increment</button>
+        <button onClick={increaseHandler}>increment by 5</button>
         <button onClick={decrementHandler}>decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
