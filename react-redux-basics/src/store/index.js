c37efb60cb143162ae473fc4ef233c2a8cc5b3fa
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-03-13 22:43:03 -0700

    added redux-tool-kit and re-wrote reducers with createSlice

--- react-redux-basics/src/store/index.js
+++ react-redux-basics/src/store/index.js
@@ -1,11 +1,44 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-
+//intial state
 const initialState = {
   counter: 0,
   showCounter: true,
 }
 
+
+//Creating a slice of global state
+//Name , initialState, and reducers that will be called in function form
+//All reducers already have up-to-date state
+//All reducers do not need actions, and will automatically know
+
+//In redux tool-kit, we are allowed to mutate the original state -- converts it under the hood
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload
+    },
+    toggleCounter (state) {
+      state.showCounter = !state.showCounter
+    }
+  }
+})
+
+
+
+/* NORMAL REDUX 
+
+//reducer which takes in all of the action types
+//NEVER MUTATE STATE, ALWAYS RETURN A NEW STATE
 const counterReducer = (state = initialState, action) => {
   if(action.type === 'increment') {
     return {
@@ -38,6 +71,13 @@ const counterReducer = (state = initialState, action) => {
   return state;
 }
 
-const store = createStore(counterReducer);
+*/
+
+//creatingt the store from the reducer
+
+//takes in a configuration object, with the reducer object
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
 
 export default store;
