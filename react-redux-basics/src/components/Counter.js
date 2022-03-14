f4bdcbbcd343328f241cf4a219e7f6fdcf2bd12b
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-03-13 21:55:56 -0700

    hooked up basic redux dispatch

--- react-redux-basics/src/components/Counter.js
+++ react-redux-basics/src/components/Counter.js
@@ -1,15 +1,34 @@
 import classes from './Counter.module.css';
 
 //useSelector allows us to target a part of the state that we want
-import { useSelector, connect } from 'react-redux';
+import { useSelector, connect, useDispatch } from 'react-redux';
 
 const Counter = () => {
+  //extracting the counter state 
+  //automatically subscribes and will reload if the state changes
+  const counter = useSelector(state => state.counter);
+
+  //Will dispatch an action to the store that we are subscribed to
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' })
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' })
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
