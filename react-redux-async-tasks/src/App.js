6543bbfd567d4dd21b14eb88690412d9142e33dc
anthonyzhu132 <anthonyzhu132@gmail.com>
2022-03-14 11:54:28 -0700

    added useEffect code and refactored to collect response and error handling

--- react-redux-async-tasks/src/App.js
+++ react-redux-async-tasks/src/App.js
@@ -1,18 +1,77 @@
+import { useEffect, Fragment } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/notifications/Notification';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/uiSlice';
+
+let isInitial = true;
 
 function App() {
-  const cartToggle = useSelector(state => state.ui.cartIsVisible)
+  const cartToggle = useSelector(state => state.ui.cartIsVisible);
+  const dispatch = useDispatch()
 
-  return (
-    <Layout>
-      {cartToggle && <Cart />}
-      <Products />
-    </Layout>
-  );
+  const notification = useSelector(state => state.ui.notification)
+
+  //setting up a subscription to cart state 
+  const cart = useSelector(state => state.cart);
+
+  //when state changes, fires http request and sends cart data to link
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(uiActions.setNotification({
+        status: 'pending',
+        title: 'sending',
+        message: 'sending data',
+      }))
+
+      const response = await fetch('https://reactcourse-d4707-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      })
+
+      if(!response.ok) {
+        throw new Error('Sending error')
+      }
+
+      dispatch(uiActions.setNotification({
+        status: 'success',
+        title: 'success',
+        message: 'sending successful',
+      }))
+    }
+
+    if (isInitial) {
+      isInitial = false;
+      return
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.setNotification({
+        status: 'error',
+        title: 'error',
+        message: 'sending error',
+      }))
+    })
+
+  }, [cart, dispatch])
+
+  return (<Fragment>
+  {notification && (
+    <Notification
+      status={notification.status}
+      title={notification.title}
+      message={notification.message}
+    />
+  )}
+  <Layout>
+    {cartToggle && <Cart />}
+    <Products />
+  </Layout>
+</Fragment>
+)
 }
 
 export default App;
